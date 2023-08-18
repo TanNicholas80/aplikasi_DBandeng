3f413bd63ac4aaba5c6791ef983d92624fbdd671
Tan Nicholas <tannicholas54@gmail.com>
2023-08-18 19:59:38 +0700

    bikin tampilan login

---
+++ app/src/main/java/com/example/dbandeng/choose_register.kt
@@ -0,0 +1,11 @@
+package com.example.dbandeng
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class choose_register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_register)
+    }
+}
