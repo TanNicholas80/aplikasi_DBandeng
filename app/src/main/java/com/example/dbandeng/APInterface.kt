fdf60e5f734d9d754c48d6ad171fc80e18b2f800
Tan Nicholas <tannicholas54@gmail.com>
2023-08-23 12:33:59 +0700

    penambahan landing page product, news, profile

--- app/src/main/java/com/example/dbandeng/APInterface.kt
+++ app/src/main/java/com/example/dbandeng/APInterface.kt
@@ -1,27 +1,23 @@
 package com.example.dbandeng
 
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
-
 interface APInterface {
     // Register User
-    @FormUrlEncoded
-    @POST()
-    // Login User
-    @FormUrlEncoded
-    @POST()
-    // Login Mitra
-    @FormUrlEncoded
-    @POST()
-    // Profile User
-    @GET()
-    // Profile Mitra
-    @GET()
-    // Edit Profile User
-    @FormUrlEncoded
-    @POST()
-    // Edit Profile Mitra
-    @FormUrlEncoded
-    @POST()
+//    @FormUrlEncoded
+//    @POST()
+//    // Login User
+//    @FormUrlEncoded
+//    @POST()
+//    // Login Mitra
+//    @FormUrlEncoded
+//    @POST()
+//    // Profile User
+//    @GET()
+//    // Profile Mitra
+//    @GET()
+//    // Edit Profile User
+//    @FormUrlEncoded
+//    @POST()
+//    // Edit Profile Mitra
+//    @FormUrlEncoded
+//    @POST()
 }
--- app/src/main/java/com/example/dbandeng/landing_page_profile.kt
+++ app/src/main/java/com/example/dbandeng/landing_page_profile.kt
@@ -5,33 +5,35 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 
-class lading_page_home : AppCompatActivity() {
-
+class landing_page_profile : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lading_page_home)
+        setContentView(R.layout.activity_landing_page_profile)
 
         val bottomNavigation = findViewById(R.id.bottomNavigation) as MeowBottomNavigation
-        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.home_icon_solid))
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.home_not_active))
         bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.product_icon))
         bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.news_user))
-        bottomNavigation.add(MeowBottomNavigation.Model(4, R.drawable.profile_user))
+        bottomNavigation.add(MeowBottomNavigation.Model(4, R.drawable.profile_active))
         bottomNavigation.setOnClickMenuListener {
-            when(it.id) {
+            when (it.id) {
                 1 -> {
-                    val landing_page_layout = Intent(this@lading_page_home, lading_page_home::class.java);
+                    val landing_page_layout = Intent(this@landing_page_profile, lading_page_home::class.java);
                     startActivity(landing_page_layout)
                 }
+
                 2 -> {
-                    val product_page_layout = Intent(this@lading_page_home, landing_page_product::class.java);
+                    val product_page_layout = Intent(this@landing_page_profile, landing_page_product::class.java);
                     startActivity(product_page_layout)
                 }
+
                 3 -> {
-                    val news_page_layout = Intent(this@lading_page_home, landing_page_news::class.java);
+                    val news_page_layout = Intent(this@landing_page_profile, landing_page_news::class.java);
                     startActivity(news_page_layout)
                 }
+
                 4 -> {
-                    val profile_user_layout = Intent(this@lading_page_home, landing_page_profile::class.java);
+                    val profile_user_layout = Intent(this@landing_page_profile, landing_page_profile::class.java);
                     startActivity(profile_user_layout)
                 }
             }
